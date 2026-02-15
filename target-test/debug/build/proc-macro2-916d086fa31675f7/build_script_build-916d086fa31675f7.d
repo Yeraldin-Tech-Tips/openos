e9f3d709521e8a7e50f3128dcad11063058f7e64
yeraldinm <yeraldinmueses@gmail.com>
2026-02-15 17:05:52 -0500

    chore: ignore and untrack target-test artifacts

--- target-test/debug/build/proc-macro2-916d086fa31675f7/build_script_build-916d086fa31675f7.d
+++
@@ -1,5 +0,0 @@
-/mnt/d/workspace/openos/target-test/debug/build/proc-macro2-916d086fa31675f7/build_script_build-916d086fa31675f7.d: /home/ymueses/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.106/build.rs
-
-/mnt/d/workspace/openos/target-test/debug/build/proc-macro2-916d086fa31675f7/build_script_build-916d086fa31675f7: /home/ymueses/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.106/build.rs
-
-/home/ymueses/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.106/build.rs:
--- target-test/debug/build/serde_json-bdff4bd535902d25/build_script_build-bdff4bd535902d25.d
+++
@@ -1,5 +0,0 @@
-/mnt/d/workspace/openos/target-test/debug/build/serde_json-bdff4bd535902d25/build_script_build-bdff4bd535902d25.d: /home/ymueses/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_json-1.0.149/build.rs
-
-/mnt/d/workspace/openos/target-test/debug/build/serde_json-bdff4bd535902d25/build_script_build-bdff4bd535902d25: /home/ymueses/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_json-1.0.149/build.rs
-
-/home/ymueses/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_json-1.0.149/build.rs:
